132d40bac69902bf9858f2edc3f08a32fc154dc6
oliviaw171 <owang@smith.edu>
2024-06-04 19:14:43 -0400

    loading existing datasets

--- patent_data.R
+++ patent_data.R
@@ -3,6 +3,7 @@
 
 # Load necessary packages
 library(haven)
+library(utils)
 
 # Loading DISCERN data (data format: .dta)
 discern_database <- read_dta("output_files/DISCERN_patent_database_1980_2015_final1.dta")
@@ -10,3 +11,12 @@ discern_panel <- read_dta("output_files/DISCERN_Panel_Data_1980_2015.dta")
 discern_SUB_name <- read_dta("output_files/DISCERN_SUB_name_list.dta")
 discern_UO_name <- read_dta("output_files/DISCERN_UO_name_list.dta")
 
+# Loading compustat-patent data (data format: .csv)
+staticTranche1 <- read.csv("compustat-patent/staticTranche1.csv")
+staticTranche2 <- read.csv("compustat-patent/staticTranche2.csv")
+staticTranche3 <- read.csv("compustat-patent/staticTranche3.csv")
+staticTranche4 <- read.csv("compustat-patent/staticTranche4.csv")
+staticTranche5 <- read.csv("compustat-patent/staticTranche5.csv")
+staticTranche6 <- read.csv("compustat-patent/staticTranche6.csv")
+staticTranche7 <- read.csv("compustat-patent/staticTranche7.csv")
+staticTranche8 <- read.csv("compustat-patent/staticTranche8.csv")
